53d07f4646283d75b543bfca2f64203e0008c38b
Infern07 <jaypandya428@gmail.com>
2024-08-26 19:12:08 +0530

    first

---
+++ tailwind.config.js
@@ -0,0 +1,10 @@
+tailwind.config = {
+  theme:{
+    gridTemplateColumns:{
+      'auto': 'repeat(auto-fit, minmax(200px, 1fr))'
+    },
+    fontFamily:{
+      PlusJakartaSans:["Plus Jakarta Sans", "system-ui"]
+    }
+  }
+}
